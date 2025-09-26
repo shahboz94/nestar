04099c26271fdd4f80ad40503d124446c1ae8965
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-09-26 11:27:00 +0900

    feat: TASK ZN

--- train.ts
+++ train.ts
@@ -1,3 +1,21 @@
+//TASK ZN:
+
+function rotateArray(arr: number[], index: number): number[] {
+	const len = arr.length;
+	const rotated = arr.slice(len - index).concat(arr.slice(0, len - index));
+	console.log(rotated);
+	return rotated;
+}
+
+rotateArray([10, 20, 30, 40, 50, 60], 2);
+
+// Shunday function yozing, uni array va number parametri bo'lsin.
+// Function'ning vazifasi ikkinchi parametr'da berilgan raqam, birinchi
+// array parametr'ning indeksi bo'yicha hisoblanib, shu indeksgacha bo'lgan
+// raqamlarni indeksdan tashqarida bo'lgan raqamlar bilan o'rnini
+// almashtirib qaytarsin.
+
+/**
 // TASK ZM:
 
 function reverseInteger(num: number): number {
@@ -19,6 +37,7 @@ reverseInteger(987654321);
 // Yuqoridagi misolda, function kiritilgan raqamlarni orqasiga
 // o'girib (reverse) qilib qaytarmoqda.
 
+**/
 /**
 
 // TASK ZK:
