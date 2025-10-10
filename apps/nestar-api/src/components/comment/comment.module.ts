1c8ffc91bdad4030bc0c794ac011ff5a5327ec3c
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-10-10 20:07:15 +0900

    feat: develop createComment, updateComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -22,5 +22,6 @@ import { BoardArticleModule } from '../board-article/board-article.module';
 		BoardArticleModule,
 	],
 	providers: [CommentResolver, CommentService],
+	exports: [CommentService],
 })
 export class CommentModule {}
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -153,7 +153,7 @@ export class MemberService {
 	public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
 		const { _id, targetKey, modifier } = input;
 		return await this.memberModel
-			.findOneAndUpdate(
+			.findByIdAndUpdate(
 				_id,
 				{
 					$inc: { [targetKey]: modifier },
