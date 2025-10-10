ab45e56334db57b19f36d89df0bfb2ef432d5f9f
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-10-11 08:00:09 +0900

    feat: create likeService model

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types/common';
+import { LikeService } from './component/like/like.service';
 
 @Module({
 	imports: [
@@ -31,6 +32,6 @@ import { T } from './libs/types/common';
 		DatabaseModule,
 	],
 	controllers: [AppController],
-	providers: [AppService, AppResolver],
+	providers: [AppService, AppResolver, LikeService],
 })
 export class AppModule {}
--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import LikeSchema from '../../schemas/Like.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LikeService } from '../../component/like/like.service';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Comment',
+				schema: LikeSchema,
+			},
+		]),
+	],
+	providers: [LikeService],
+	exports: [LikeService],
+})
 export class LikeModule {}
