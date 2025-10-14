c781ceea17d4e57bb52a1858661cfc09701aac78
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-10-14 18:32:32 +0900

    feat: develop getVisited  GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -87,6 +87,16 @@ export class PropertyResolver {
 		return await this.propertyService.getFavorities(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Query(() => Properties)
+	public async getVisited(
+		@Args('input') input: OrdinaryInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getVisited');
+		return await this.propertyService.getVisited(memberId, input);
+	}
+
 	@Roles(MemberType.AGENT)
 	@UseGuards(RolesGuard)
 	@Query((returns) => Properties)
